1d7f79d43b6172517a840772efb247e530c3533b
Victor Portella <visportella@gmail.com>
2014-05-08 22:37:19 -0300

    Criação do grafo quase pronta. Falta acertar cálculo das distâncias e comparação

--- teste1.cpp
+++ teste1.cpp
@@ -1,6 +1,9 @@
 #include <cstdio>
 #include <cstdlib>
+
+#include <list>
 #include <vector>
+#include <algorithm>
 
 
 using namespace std;
@@ -13,31 +16,69 @@ using namespace std;
 
 */
 
+
+
+
+typedef struct ponto{
+	int x;
+	int y;
+	int i;
+} *Ponto;
+
+
+int pontoVertice(Ponto p);
 int** alocaMatrizQuadrada(int n);
 void criaGrafo();
-void criaArestas(int,int);
+void criaArestas(Ponto);
 void novaAresta(int, int);
-float distCircular(int,int,int,int);
+float distCircular(Ponto, Ponto);
 
 
 float d, D;
-int n;
+int n, N;
 int** A;
 
-vector<int>* adj;
+list<int> adj[1000];
+Ponto* vertices;
 
 
 int main(int argc, char* argv[])
 {
 
 
+
 	D = strtof(argv[1], NULL);
 	d = strtof(argv[2], NULL);
 	n = D/d; /* cuidado com arredondamento */
 
-	A = alocaMatrizQuadrada(n);
+	//adj = new list<int>[n];
 
-	adj =(vector<int>*) malloc(n*sizeof(vector<int>));
+	A = alocaMatrizQuadrada(n);
+	N = n*n;
+
+	//adj = new vector<int>[n];
+	vertices = new Ponto[n*n];
+
+	Ponto pteste;
+
+	for(int i = 0; i < n; i++)
+		for( int j = 0; j < n; j++)
+		{
+			A[i][j] = 1;
+			Ponto p = (Ponto) malloc(sizeof(ponto));
+			p->x = j; p->y = i; p->i = pontoVertice(p);
+			if(i == n/2 && j == n/2) pteste = p;
+			vertices[p->i] = p;
+		}
+
+	criaArestas(pteste);
+
+	for(int i = 0; i < n; i++)
+	{
+		for(int j = 0; j < n; j++)
+			printf("[%d]",A[i][j]);
+		printf("\n");
+	}
 
 
 	return 0;
@@ -49,9 +90,9 @@ int main(int argc, char* argv[])
 	da esquerda para a direita
 */
 
-int pontoVertice(int x, int y)
+int pontoVertice(Ponto p)
 {
-	return n*y + x;
+	return n*p->y + p->x;
 }
 
 /*****************
@@ -63,15 +104,20 @@ int pontoVertice(int x, int y)
 
 *********************/
 
-float distCircular(int x1, int y1, int x2, int y2)
+float distCircular(Ponto p1, Ponto p2)
 {
+	int x1 = p1->x, y1 = p1->y, x2 = p2->x, y2 = p2->y;
 	float dx = x1 -x2, dy = y1 - y2;
+
+	if(dx < 0) dx*=-1;
+	if(dy < 0) dy*=-1;
 
 	if(dx > n/2 ) // Rever
 		dx = n - dx;
 	if(dy > n/2) // Rever
 		dy = n - dy;
 
+	//printf("Dist: %f\n", (dx*dx + dy*dy)*d);
 	return (dx*dx + dy*dy)*d;
 }
 
@@ -86,32 +132,40 @@ float distCircular(int x1, int y1, int x2, int y2)
 
 void criaGrafo()
 {
-	for(int i = 0; i < n; i++)
-		for(int j = 0; j < n; j++)
-			criaArestas(i,j);
+	for(int i = 0; i < N; i++)
+		criaArestas(vertices[i]);
 }
 
 
 
 
-void criaArestas(int x, int y)
+void criaArestas(Ponto p)
 {
-	for(int i = 0; i < n; i++)
-		for(int j = 0; j <  n; j++)
-		{
-			float dist = distCircular(x,y,i,j);
-			if(dist >= 1 && dist <= 1+d) // REVER
-				novaAresta(pontoVertice(x,y),pontoVertice(i,j));
-				// Cria aresta entre (x,y) e (i,j)
+	A[p->x][p->y] = 0;
+	for(int i = 0; i < N; i++)
+	{
+		Ponto p2 = vertices[i];
+		float dist = distCircular(p,p2);
+		if(dist >= 1-d && dist <= 1+d){ // REVER
+			novaAresta(p->i,p2->i);
+			A[p2->x][p2->y] = 0;
 		}
+			// Cria aresta entre p1 e p2
+	}
 
 }
 
 
 void novoArco(int v1, int v2)
 {
-	//Falta verificar de v1->v2 ja existe
-	ajd[v1].push_back(v2);
+
+	for (list<int>::iterator it = adj[v1].begin() ; it != adj[v1].end(); ++it)
+	{
+		if(*it == v2) return;
+	}
+	//if(find(adj[v1].begin(), adj[v1].end(), v2) != adj[v1].end())
+	printf("HUE\n");
+	adj[v1].push_back(v2);
 }
 
 
